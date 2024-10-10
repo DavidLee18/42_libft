1d851fb91463c45bb3755bb19be7a1324f8f0a5e
Jaehyun Lee <jaehylee@c1r24s1.42gyeongsan.kr>
2024-10-10 15:30:05 +0900

    fix atoi + striteri sketch

--- ft_atoi.c
+++ ft_atoi.c
@@ -16,20 +16,22 @@ int
 	ft_atoi(const char *str)
 {
 	int	i;
-	int	is_neg;
+	int	sign;
 	int	res;
 
-	if (!str)
+	if (!str[0] || !str)
 		return (0);
 	i = 0;
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 		i++;
-	is_neg = (str[i] == '-') * 2 - 1;
-	if (is_neg == -1 || str[i] == '+')
+	sign = (str[i] != '-') * 2 - 1;
+	if (sign == -1 || str[i] == '+')
 		i++;
 	res = 0;
 	while (str[i] >= '0' && str[i] <= '9')
-		res = (res * 10) + (str[i++] - '0');
-	return (res * is_neg);
+	{
+		res = (res * 10) + sign * (str[i++] - '0');
+	}
+	return (res);
 }
